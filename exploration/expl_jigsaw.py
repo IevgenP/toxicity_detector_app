b7e43cc21fb3bde4053d509528a8b7b25a819f6e
Ievgen Potapenko <ievgenpotapenko@MacBook-Pro-Ievgen.local>
2019-05-16 17:13:18 +0300

    Update visualization of word frequency with tf-idf approach.

--- exploration/expl_jigsaw.py
+++ exploration/expl_jigsaw.py
@@ -1,6 +1,7 @@
 
 #%%
 import nltk
+import numpy as np
 import collections
 import warnings
 from sklearn.pipeline import Pipeline
@@ -59,3 +60,89 @@ plt.show()
 
 #%%
 # Each plot shows that toxic comments tend to be short.
+
+#%%
+# Download stop words from nltk library
+nltk.download('stopwords')
+eng_stop_words = nltk.corpus.stopwords.words('english') 
+
+
+#%%
+# Add column for text transformations
+j_df['prep_text'] = j_df['comment_text'].copy()
+
+# Create pipeline for transformation
+pipeline = Pipeline(
+    steps=[
+        ('contracted', ShortToLong(['prep_text'])),
+        ('punctuation', PunctuationRemover(['prep_text'])),
+        ('lowercase', IntoLowerCase(['prep_text'])),
+        ('stopwords', StopWordsRemover(['prep_text'], eng_stop_words))
+    ]
+)
+
+#%%
+# Transform the dataset using created pipeline
+pipeline.fit_transform(j_df)
+j_df.to_csv(ROOT_DIR + '/temp_data/jigsaw_df_no_stop_words.csv', index=False)
+
+#%%
+# The following analysis must use tf-idf otherwise there are cases where comment 
+# consists of one word only repeated many times (example: "fucksex").
+
+
+#%%
+# Count words frequency for toxic comments only
+count_words(j_df.loc[j_df['toxic'] == 1, ], 'prep_text', 'unigrams', 15, ' for toxic comments')
+plt.show()
+count_words(j_df.loc[j_df['toxic'] == 1, ], 'prep_text', 'bigrams', 15, ' for toxic comments')
+plt.show()
+
+#%%
+# Count words frequency for severe toxic comments only
+count_words(j_df.loc[j_df['severe_toxic'] == 1, ], 'prep_text', 'unigrams', 15, ' for severe toxic comments')
+plt.show()
+count_words(j_df.loc[j_df['severe_toxic'] == 1, ], 'prep_text', 'bigrams', 15, ' for severe toxic comments')
+plt.show()
+
+#%%
+#%%
+# Count words frequency for obscene comments only
+count_words(j_df.loc[j_df['obscene'] == 1, ], 'prep_text', 'unigrams', 15, ' for obscene comments')
+plt.show()
+count_words(j_df.loc[j_df['obscene'] == 1, ], 'prep_text', 'bigrams', 15, ' for obscene comments')
+plt.show()
+
+#%%
+# Count words frequency for threat comments only
+count_words(j_df.loc[j_df['threat'] == 1, ], 'prep_text', 'unigrams', 15, ' for threat comments')
+plt.show()
+count_words(j_df.loc[j_df['threat'] == 1, ], 'prep_text', 'bigrams', 15, ' for threat comments')
+plt.show()
+
+
+#%%
+# Count words frequency for insult comments only
+count_words(j_df.loc[j_df['insult'] == 1, ], 'prep_text', 'unigrams', 15, ' for insult comments')
+plt.show()
+count_words(j_df.loc[j_df['insult'] == 1, ], 'prep_text', 'bigrams', 15, ' for insult comments')
+plt.show()
+
+
+#%%
+# Count words frequency for identity_hate comments only
+count_words(j_df.loc[j_df['identity_hate'] == 1, ], 'prep_text', 'unigrams', 15, ' for identity hate comments')
+plt.show()
+count_words(j_df.loc[j_df['identity_hate'] == 1, ], 'prep_text', 'bigrams', 15, ' for identity hate comments')
+plt.show()
+
+
+
+#%%
+j_df.loc[j_df['prep_text'].str.contains('fucksex'), 'comment_text'].values
+
+
+#%%
+np.log10(100/95+0.00001)
+
+#%%
--- src/visualizers/visualiers.py
+++ src/visualizers/visualiers.py
@@ -3,6 +3,7 @@ import collections
 import warnings
 from sklearn.pipeline import Pipeline
 import pandas as pd
+import numpy as np
 import seaborn as sns
 from definitions import ROOT_DIR
 
@@ -30,28 +31,53 @@ def get_stats(df, colum_content, column):
 def plot_counter(counter_output, top_n, n_gram, text):
     """Function for getting the most common words for both target values"""
     count_df = pd.DataFrame.from_dict(counter_output, orient='index').reset_index()
-    count_df = count_df.rename(columns={'index': 'word', 0: 'count'})
-    count_df = count_df.sort_values(by='count', axis=0, ascending=False)
-    sns.barplot(x="count", y="word", data=count_df.head(top_n), palette="Blues_d").set_title('{} frequency count{}.'.format(n_gram.capitalize(), text))
+    count_df = count_df.rename(columns={'index': 'word', 0: 'TF-IDF count'})
+    count_df = count_df.sort_values(by='TF-IDF count', axis=0, ascending=False)
+    sns.barplot(x="TF-IDF count", y="word", data=count_df.head(top_n), palette="Blues_d").set_title('{} IF-IDF count{}.'.format(n_gram.capitalize(), text))
 
 
 def count_words(df, column, n_gram, top_n, text=None):
     """Function for counting most common words words"""
 
     count = collections.Counter()
-    if n_gram == 'unigrams':
-        for row in df[column]:
-            words = nltk.word_tokenize(row)
+    tf_idf_dict = {}
+    for row in df[column]:
+        words = nltk.word_tokenize(row)
+        if n_gram == 'unigrams':
+            
+            # compute raw term frequency of each words in dataframe column
             count.update(words)
-    elif n_gram == 'bigrams':
-        for row in df[column]:
-            words = nltk.word_tokenize(row)
+            
+            # compute soft inverse document frequency
+            for word in count:
+                idf = np.log(float(df.shape[0])/(len([1 for row in df[column] if word in row])+0.001) + 0.001)
+                tf_idf_dict[word] = count[word] * idf
+        
+        elif n_gram == 'bigrams':
+            
+            # compute raw term frequency of each words in dataframe column
             count.update(nltk.bigrams(words))
-    elif n_gram == 'trigrams':
-        for row in df[column]:
-            words = nltk.word_tokenize(row)
-            count.update(nltk.trigrams(words))
-    else:
-        warnings.warn("Not viable value for n_gram is provided. Please select one of the following: 'unigrams', 'bigrams', 'trigrams'.")
+            
+            # compute soft inverse document frequency
+            for ngram in count:
+                comb = " ".join(list(ngram))
+                idf = np.log(float(df.shape[0])/(len([1 for row in df[column] if comb in row])+0.001) + 0.001)
+                tf_idf_dict[ngram] = count[ngram] * idf
+        
+        elif n_gram == 'trigrams':
 
-    plot_counter(count, top_n, n_gram, text)
+            # compute raw term frequency of each words in dataframe column
+            count.update(nltk.trigrams(words))
+
+            # compute soft inverse document frequency
+            for ngram in count:
+                comb = " ".join(list(ngram))
+                idf = np.log(float(df.shape[0])/(len([1 for row in df[column] if comb in row])+0.001) + 0.001)
+                tf_idf_dict[ngram] = count[ngram] * idf
+        
+        else:
+            warnings.warn("Not viable value for n_gram is provided. Please select one of the following: 'unigrams', 'bigrams', 'trigrams'.")
+    
+    
+    
+    plot_counter(tf_idf_dict, top_n, n_gram, text)
